60c6d63e5b6f0f31fcb59377e0b38a473d1741ac
Vrigz Alejo <nuedu2012@gmail.com>
2015-07-18 21:19:18 +0800

    done LongArrayDiff

--- tests/LongArrayDiffTest.php
+++ tests/LongArrayDiffTest.php
@@ -1,15 +1,19 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: vrigzlinuxmint13
  * Date: 7/18/15
  * Time: 8:43 PM
  */
-
-class LongArrayDiffTest extends PHPUnit_Framework_TestCase {
+class LongArrayDiffTest extends PHPUnit_Framework_TestCase
+{
 
     public function testEquality()
     {
-
+        $this->assertEquals(
+            array( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6 ),
+            array( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 33, 4, 5, 6 )
+        );
     }
 }
